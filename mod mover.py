20a651043297f9159e06410203f721aff6807cb0
necrossady <cassadybrunette@gmail.com>
2022-02-05 15:25:28 -0500

    add file
    add file for script

---
+++ mod mover.py
@@ -0,0 +1,14 @@
+import os
+
+def validateRequest(fromSlot, toSlot, modFolder):
+    # ensure slots are 0-7
+    # ensure modFolder exists
+    # ensure modFolder has a mod in slot from
+    # ensure modFolder does not already have a mod in slot to
+    pass
+
+def getFilesToChange(fromSlot, folder):
+    # go through folder:
+    #   get c0X folder where X is fromSlot
+    #   change _0X files where X is fromSlot
+    pass
