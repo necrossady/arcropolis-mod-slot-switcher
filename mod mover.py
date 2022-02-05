86dd42607470648aba1930051f577ea148956051
necrossady <cassadybrunette@gmail.com>
2022-02-05 17:18:46 -0500

    get user input

--- mod mover.py
+++ mod mover.py
@@ -1,16 +1,19 @@
 import os
 import glob
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
 
 
 def validateRequest(fromSlot, toSlot, modFolder):
     # ensure slots are 0-7
-    if (
-        not(isinstance(fromSlot,int) and isinstance(toSlot,int))
-        or fromSlot < 0 or toSlot < 0
-        or fromSlot > 7 or toSlot > 7
+    if not(isinstance(fromSlot,int) and isinstance(toSlot,int)):        
+        return False, "slots must be integers"
+    elif (
+        fromSlot < 0 or toSlot < 0 or
+        fromSlot > 7 or toSlot > 7
         ):
-        return False, "slots must be integers 0-7"
-    
+        return False, "slots must be 0-7"
+            
     # ensure modFolder exists
     # ??? this will fail for UI-only mods, will need to address that if it should work with them
     cglob = "**\\[cC]0{}\\".format(fromSlot)
@@ -29,6 +32,19 @@ def validateRequest(fromSlot, toSlot, modFolder):
         return False, "{} already exists".format(toPath)
 
     return True, ""
+
+def getModFolder():
+    print("select the mod folder")
+    Tk().withdraw()
+    modFolder = askdirectory(title="Select the root mod folder")
+    return modFolder
+
+def getSlots():
+    f = input("Move from which slot? ")
+    t = input("Move to which slot? ")
+    f = int(f)
+    t = int(t)
+    return f,t
 
 def getFilesToChange(fromSlot, folder):
     # go through folder:
